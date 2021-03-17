1c8cdce8c2a9cd8fe60dcd847f24e26b53952e52
lcmarval <luiscarm77@gmail.com>
2021-03-17 00:36:38 -0400

    added datepicker validations

--- src/app/reservas-especiales/form-reservas-especiales/form-reservas-especiales.component.ts
+++ src/app/reservas-especiales/form-reservas-especiales/form-reservas-especiales.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AppService } from 'app/app.service';
 
 @Component({
   selector: 'app-form-reservas-especiales',
@@ -9,10 +10,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class FormReservasEspecialesComponent implements OnInit {
   public form: FormGroup;
   public hours: any[];
+  public minDate: Date;
+  public maxDate: Date;
   @Input() laboratorios: any[];
   @Output() formValues = new EventEmitter();
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private service: AppService) { }
 
   ngOnInit() {
     this.createForm();
@@ -21,9 +24,11 @@ export class FormReservasEspecialesComponent implements OnInit {
       {id: '11:00AM'},{id: '12:00AM'},{id: '1:00PM'},{id: '2:00PM'},
       {id: '3:00PM'},{id: '4:00PM'},{id: '5:00PM'},{id: '6:00PM'},
     ];
+    this.getEndTrimestre();
   }
 
   createForm() {
+    this.minDate = new Date();
     this.form = this.formBuilder.group({ 
       contact_name: [null, [Validators.required]], 
       contact_email: [null, [Validators.required, Validators.email, Validators.pattern('([a-z,0-9,-]+@usb\.ve)')]],
@@ -33,6 +38,10 @@ export class FormReservasEspecialesComponent implements OnInit {
       amount_people: [null, [Validators.required, Validators.pattern('([0-9]+)')]],
       observations: [null] 
     });
+  }
+
+  getEndTrimestre() {
+    this.service.getTrimester().subscribe(response => this.maxDate = response[0].finish )
   }
 
   onFormChange(event?: any) {
